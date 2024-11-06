8b43b63cb236c41562b96db2ed71c3baa191c8b2
SeanHub-13 <coolflame68@gmail.com>
2024-11-05 19:45:58 -0500

    Added Basic 8x8 Grid and basic code styling
    Added an 8x8 square grid that is half the size of the canvas

--- js/script.js
+++ js/script.js
@@ -1,24 +1,50 @@
 /**
- * Title of Project
- * Author Name
+ * !Grid Match!
+ * Sean Verba
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Small 16x16 grid, flashes a picture made on the grid to the player, then asks them to recreate it as best as they can draw it from memory.
+ * Compares how well they did based on what grid tiles they drew on, then gives a score.
+ * If the player gets a score above a certain amount each round, they continue, if not, trigger game over leaderboard and ask them for a name so that their score gets added
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+const grid = {
+    columns: 8,
+    rows: 8,
+    squareW: null,
+    squareH: null,
+    fill: {
+        r: 255,
+        g: 255,
+        b: 255
+    }
+}
+
+// Setup runs code on start-up
 function setup() {
+    createCanvas(800, 800);
+    grid.squareW = width / grid.columns / 2;
+    grid.squareH = height / grid.rows / 2;
+    drawGrid();
+}
+
+// Draw runs code every frame
+function draw() {
 
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
+function drawGrid() {
+    // Let i equal 0, for every time i equals less than 8, add 1 to i
+    for (let i = 0; i < grid.columns; i++) {
+        for (let j = 0; j < grid.rows; j++) {
+            let x = i * grid.squareW;
+            let y = j * grid.squareH;
+            push();
+            stroke(0);
+            fill(grid.fill.r, grid.fill.g, grid.fill.b);
+            rect(x, y, grid.squareW, grid.squareH);
+            pop();
+        }
+    }
 }
