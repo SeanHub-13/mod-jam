a40b196c47008738b79b0adfd34b361b6c94159b
SeanHub-13 <coolflame68@gmail.com>
2024-11-05 21:59:25 -0500

    Added Background, Grid Offset, Tracking Mouse Click Location on Grid
    - Added a background color to separate the grid from the background
    - Added an offset for where the grid is drawn (centered it on x and put it down a bit more)
    - Added function that finds where the player clicked their mouse on the grid

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
  * !Grid Match!
  * Sean Verba
  * 
- * Small 16x16 grid, flashes a picture made on the grid to the player, then asks them to recreate it as best as they can draw it from memory.
+ * Small 8x8 grid, flashes a picture made on the grid to the player, then asks them to recreate it as best as they can draw it from memory.
  * Compares how well they did based on what grid tiles they drew on, then gives a score.
  * If the player gets a score above a certain amount each round, they continue, if not, trigger game over leaderboard and ask them for a name so that their score gets added
  */
@@ -14,6 +14,8 @@ const grid = {
     rows: 8,
     squareW: null,
     squareH: null,
+    offsetX: 200,
+    offsetY: 300,
     fill: {
         r: 255,
         g: 255,
@@ -24,6 +26,7 @@ const grid = {
 // Setup runs code on start-up
 function setup() {
     createCanvas(800, 800);
+    background(255, 150, 170);
     grid.squareW = width / grid.columns / 2;
     grid.squareH = height / grid.rows / 2;
     drawGrid();
@@ -31,20 +34,34 @@ function setup() {
 
 // Draw runs code every frame
 function draw() {
-
+    gridClickCheck();
 }
 
 function drawGrid() {
     // Let i equal 0, for every time i equals less than 8, add 1 to i
     for (let i = 0; i < grid.columns; i++) {
         for (let j = 0; j < grid.rows; j++) {
-            let x = i * grid.squareW;
-            let y = j * grid.squareH;
+            // Number of i (squares) * squares width + offset
+            let x = i * grid.squareW + grid.offsetX;
+            let y = j * grid.squareH + grid.offsetY;
             push();
             stroke(0);
             fill(grid.fill.r, grid.fill.g, grid.fill.b);
             rect(x, y, grid.squareW, grid.squareH);
             pop();
+        }
+    }
+}
+
+function gridClickCheck() {
+    if (mouseIsPressed) {
+        // Adjusts mouse for grid offset before / by grid square sizes to find which cell the mouse is on
+        let columnMouse = ((mouseX - grid.offsetX) / grid.squareW);
+        let rowMouse = ((mouseY - grid.offsetY) / grid.squareH);
+        // If columnMouse (The column that the mouse is on) is more or equal to 0 AND columnMouse is less than the number of columns
+        // Then, if mouse is greater than or equal to offset AND mouse is less than or equal to offset + number of columns * cell size - basically is the mouse less than or equal to the offset + the grid
+        if (columnMouse >= 0 && columnMouse < grid.columns && rowMouse >= 0 && rowMouse < grid.rows && mouseX >= grid.offsetX && mouseX <= grid.offsetX + grid.columns * grid.squareW && mouseY >= grid.offsetY && mouseY <= grid.offsetY + grid.rows * grid.squareH) {
+            console.log(columnMouse, rowMouse);
         }
     }
 }
