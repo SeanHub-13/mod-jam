7b7d776ddcc7979598ec64b656f2d953cb818ef2
SeanHub-13 <coolflame68@gmail.com>
2024-11-06 14:36:08 -0500

    Added Game Timer and Rudimentary Randomized Levels
    - Added a timer that slowly counts down and does something when reaching 0 (triggers level change, adds time onto the timer)
    - Added a level system that shows the player a randomized grid they cannot edit for a few seconds, saves the values under a duplicate array, then goes to the players drawing level.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@
 
 "use strict";
 
+let buttonImage = undefined;
+let buttonPressedImage = undefined;
+
 const grid = {
     columns: 8,
     rows: 8,
@@ -28,33 +31,49 @@ const grid = {
     }
 }
 
+let playButton = {
+    width: 192,
+    height: 72
+}
+
+// Array later made into a 2d array to represent cells in the grid
 let gridArray = [];
+let gridCompare = [];
+
+// State variable, currently set to titlescreen on launch
+let state = "titlescreen";
+
+// Boolean variable sets whether the player can edit the grid
+let editState = false;
+
+let timer = 0;
+
+let selectedEasy = null;
+
+let stateLevel = "one";
+
+function preload() {
+    buttonImage = loadImage('assets/images/Mod_Jam_Button_1.png');
+    buttonPressedImage = loadImage('assets/images/Mod_Jam_Button_2.png');
+}
 
 // Setup runs code on start-up
 function setup() {
     createCanvas(800, 800);
-    background(255, 150, 170);
     grid.squareW = width / grid.columns / 2;
     grid.squareH = height / grid.rows / 2;
-    // Let i equal 0, for every time i equals less than 8, add 1 to i
-    for (let i = 0; i < grid.columns; i++) {
-        // Basically creates 8 arrays (8 because of the amount of columns)
-        gridArray[i] = [];
-        for (let j = 0; j < grid.rows; j++) {
-            // Creates 8 variables (rows) inside each of the 8 arrays (columns) and then sets them all to false
-            // This sucked, screw 2d arrays, thank god for youtube explanations on this crap.
-            gridArray[i][j] = false;
-        }
-    }
-    drawGrid();
+    gridReset();
     // Line of code taken from user Zuul on stack overflow - more info in README
     // From what I can understand, this adds an event handler to the canvas that is triggered on right click and stops the context menu from appearing
     canvas.oncontextmenu = function (e) { e.preventDefault(); e.stopPropagation(); }
+    setInterval(timerUpdate, 1000);
 }
 
 // Draw runs code every frame
 function draw() {
+    background(255, 150, 170);
     gridClickCheck();
+    stateChange();
 }
 
 function drawGrid() {
@@ -88,8 +107,8 @@ function gridClickCheck() {
         let columnMouse = Math.floor((mouseX - grid.offsetX) / grid.squareW);
         let rowMouse = Math.floor((mouseY - grid.offsetY) / grid.squareH);
         // If columnMouse (The column that the mouse is on) is more or equal to 0 AND columnMouse is less than the number of columns
-        // Then, if mouse is greater than or equal to offset AND mouse is less than or equal to offset + number of columns * cell size - basically is the mouse less than or equal to the offset + the grid
-        if (columnMouse >= 0 && columnMouse < grid.columns && rowMouse >= 0 && rowMouse < grid.rows && mouseX >= grid.offsetX && mouseX <= grid.offsetX + grid.columns * grid.squareW && mouseY >= grid.offsetY && mouseY <= grid.offsetY + grid.rows * grid.squareH) {
+        // Then, if mouse is greater than or equal to offset AND mouse is less than or equal to offset + number of columns * cell size - basically is the mouse less than or equal to the offset + the grid AND the player can edit
+        if (columnMouse >= 0 && columnMouse < grid.columns && rowMouse >= 0 && rowMouse < grid.rows && mouseX >= grid.offsetX && mouseX <= grid.offsetX + grid.columns * grid.squareW && mouseY >= grid.offsetY && mouseY <= grid.offsetY + grid.rows * grid.squareH && editState === true) {
             // console.log(columnMouse + 1, rowMouse + 1);
             // If mouse button that was pressed is left
             if (mouseButton === LEFT) {
@@ -101,7 +120,121 @@ function gridClickCheck() {
                 // Sets the state of the clicked boolean to true
                 gridArray[columnMouse][rowMouse] = false;
             }
-            drawGrid();
+            // drawGrid();
         }
     }
+}
+
+function drawPlayButton() {
+    // width & height is calculated like this because I want the button to be truly centered, and the image is 192 x 72
+    image(buttonImage, width / 2 - playButton.width / 2, height / 2 - playButton.height / 2, playButton.width, playButton.height);
+    text("PLAY", width / 2, height / 2);
+    textSize(12);
+}
+
+function drawPressedPlayButton() {
+    // width & height is calculated like this because I want the button to be truly centered, and the image is 192 x 72
+    image(buttonPressedImage, width / 2 - playButton.width / 2, height / 2 - playButton.height / 2, playButton.width, playButton.height);
+    text("PLAY", width / 2, height / 2);
+    textSize(12);
+}
+
+function timerDisplay() {
+    fill(255, 255, 255);
+    text(timer, width / 2, height / 5);
+    textSize(12);
+}
+
+function playButtonInput() {
+    // If the mouse is between these values then
+    if ((mouseX > width / 2 - playButton.width / 2) &&
+        (mouseX < width / 2 + playButton.width / 2) &&
+        (mouseY > height / 2 - playButton.height / 2) &&
+        (mouseY < height / 2 + playButton.height / 2)) {
+        //If mouse hovers over the button, draw the pressed version
+        drawPressedPlayButton();
+        //Checks if the button is left clicked
+        if (mouseIsPressed) {
+            //Resets score
+            //score = 0;
+            state = "game";
+        }
+    }
+}
+
+function timerUpdate() {
+    timer--;
+}
+
+function nextStage() {
+    if (timer <= 0) {
+        if (stateLevel === "one") {
+            timer = 10;
+            editState = false;
+            selectedEasy = Math.floor(random(5, 15));
+            levelOne();
+            stateLevel = "onedraw";
+        }
+        else if (stateLevel === "onedraw") {
+            editState = true;
+            gridReset();
+            timer = 10;
+            stateLevel = "one";
+        }
+    }
+}
+
+function levelOne() {
+    gridReset();
+    for (let k = 0; k < selectedEasy; k++) {
+        let randomColumn = Math.floor(random(0, grid.columns - 1));
+        let randomRow = Math.floor(random(0, grid.rows - 1));
+        gridArray[randomColumn][randomRow] = true;
+        gridCompare = gridArray;
+    }
+}
+
+function gridReset() {
+    // Let i equal 0, for every time i equals less than 8, add 1 to i
+    for (let i = 0; i < grid.columns; i++) {
+        // Basically creates 8 arrays (8 because of the amount of columns)
+        gridArray[i] = [];
+        for (let j = 0; j < grid.rows; j++) {
+            // Creates 8 variables (rows) inside each of the 8 arrays (columns) and then sets them all to false
+            // This sucked, screw 2d arrays, thank god for youtube explanations on this crap.
+            gridArray[i][j] = false;
+        }
+    }
+}
+
+function cellCompare() {
+
+}
+
+function stateChange() {
+    //Checks what state the game is supposed to be in and changes it
+    if (state === "titlescreen") {
+        titleScreen();
+    }
+    else if (state === "game") {
+        game();
+    }
+    else if (state === "end") {
+        //end();
+    }
+}
+
+//Displays all title screen related functions
+function titleScreen() {
+    //titleScreenText1();
+    //titleScreenText2();
+    drawPlayButton();
+    playButtonInput();
+}
+
+function game() {
+    drawGrid();
+    gridClickCheck();
+    timerDisplay();
+    nextStage();
 }
